ec0bbdf280ee51fa2a22b33457ca2b186fd19aa8
numme <nummer14@naver.com>
2025-06-05 09:38:40 +0900

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            if (i == 7) {
-                break;
+            if (i % 3 != 0) {
+                continue;
             }
 
             System.out.println(i);
